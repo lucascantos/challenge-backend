09e5340ba5d73be7349f0042f5c61f608accd352
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-06-03 15:44:42 -0300

    Convertido tudo em classe Tudo funcionando

--- core.py
+++ core.py
@@ -18,48 +18,56 @@ def error_handler(function):
             pass
     return wrapper
 
+class core_table(object):
+    def __init__(self,stations, start_date, end_date):
+        self.stations = stations
+        self.start_date = start_date
+        self.end_date = end_date
 
-def datarange(start_date, end_date):
-    '''
-    Faz loop entre duas datas, diariamente
-    '''
-    for n in range(int ((end_date - start_date).days)+1):
-        yield start_date + timedelta(n)
+        self.header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
+        'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
+        'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
 
-
-@error_handler
-def database (stations, start_date, end_date, *, db_folder='stations/stations'):
-    '''
-    Acessa arquivos do banco de dados.
-    '''
-    for single_station in stations:
-        for single_date in datarange(start_date, end_date):
-            file_path = '{db_folder}/{station}/{year}/{year}-{month}-{day}.txt.zip'.format(
-             db_folder=db_folder,
-             station=single_station,
-             year=single_date.strftime('%Y'),
-             month=single_date.strftime('%m'),
-             day=single_date.strftime('%d'))
-            yield(unpack_data(file_path))
-
-@error_handler
-def unpack_data(file_path):
-    '''
-    Descompacta o arquivo e prepara um DataFrame
-    '''
-    header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
-     'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
-     'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
-
-    station_df = pd.read_csv(file_path,header=None, delim_whitespace=True)
-    station_df.columns = header
-    return station_df
+    def datarange(self):
+        '''
+        Faz loop entre duas datas, diariamente.
+        yield: uma data dentro do periodo
+        '''
+        for one_day in range(int ((self.end_date - self.start_date).days)+1):
+            yield self.start_date + timedelta(one_day)
 
 
-stations = ['A003', 'A014', 'A015', 'A011', 'A009']
-start_date = date(2014, 10, 20)
-end_date = date(2014, 10, 23)
-for i in database(['A003'], start_date, end_date):
-    # Ve se o objeto encontrado é um dataframe
-    if isinstance(i, pd.DataFrame):
-        print(i.head())
+    @error_handler
+    def make_file_path (self, *, db_folder='stations/stations'):
+        '''
+        Cria o caminho do arquivo a ser aberto.
+        '''
+        for single_station in self.stations:
+            for single_date in self.datarange():
+                self.file_path = '{db_folder}/{station}/{year}/{year}-{month}-{day}.txt.zip'.format(
+                db_folder=db_folder,
+                station=single_station,
+                year=single_date.strftime('%Y'),
+                month=single_date.strftime('%m'),
+                day=single_date.strftime('%d'))
+                yield self.file_path
+
+    @error_handler
+    @staticmethod
+    def unpack_data(self):
+        '''
+        Descompacta o arquivo e prepara um DataFrame
+        '''
+        for file_path in self.make_file_path():
+            station_df = pd.read_csv(file_path,header=None, delim_whitespace=True)
+            station_df.columns = self.header
+            yield station_df
+
+
+# stations = ['A003', 'A014', 'A015', 'A011', 'A009']
+# start_date = date(2014, 10, 20)
+# end_date = date(2014, 10, 23)
+# for i in database(['A003'], start_date, end_date):
+#     # Ve se o objeto encontrado é um dataframe
+#     if isinstance(i, pd.DataFrame):
+#         print(i.head())
--- stations.py
+++ stations.py
@@ -17,59 +17,114 @@ def timer(function):
         print('Elapsed: {}s'. format(after-before))
     return wrapper
 
-def station_name():
-    # Coluna 1 = Nome das estacoes = Pega o nome da cada pasta
-    stations_list = os.listdir('stations/stations')
-    return stations_list
-
-def station_date(station):
-    # Coluna2 = Data recente
-    # abre pasta com o maior ano
-    path = 'stations/stations/{}'.format(station)
-    years = os.listdir(path)
-    years = map(int,years)
-    biggus = -1
-    for year in years:
-        biggus = year if year > biggus else biggus
-
-    # Pegar uma lista de nome dos arquivos
-    # remove o .txt.zip
-    path = '{}/{}'.format(path, str(biggus))
-    dates = os.listdir(path)
-    latest = datetime(1800,1,1)
-    for date in dates:
+def error_handler(function):
+    '''
+    Função de tratamento dos erros da função:
+    TODO: Aprender dar um argumento de entrada no decorador pra customizar os erros por função
+    TODO: Depois disso, fazer o log com os erros
+    '''
+    def wrapper(*args, **kargs):
         try:
-            # converte o restante pra pd_datetime ou date(YYYY,MM,DD)
-            date = datetime.strptime(date[:-8], '%Y-%m-%d')
-            latest = date if date > latest else latest
-        except:
-            # por hora, vou ignorar datas bugadas (IE: 30/02/20XX)
+            return function(*args, **kargs)
+        except Exception as e:
+            # print(e)
             pass
-    # pega o arquivo mais velho
-    return biggus, latest
-
-def unpack_data(station,year, date):
-    # Coluna3 = Hora recente
-    # Abre o arquvio com a data mais recente
-    file_path = 'stations/stations/{}/{}/{}.txt.zip'.format(station, year, date)
-    header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
-     'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
-     'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
-
-    station_df = pd.read_csv(file_path, header=None, delim_whitespace=True)
-    station_df.columns = header
-    latest_time = station_df['HORA'][-1]
-    print(latest_time)
-    # converte a coluna ['HORA'] pra pd.to_datetime ou algo do tipo
-    # pega o valor mais recente OU pega ultima linha
+    return wrapper
 
 
-for station in station_name():
-    try:    
-        year, lastdate = station_date(station)
-        lastdate = datetime.strftime(lastdate, '%Y-%m-%d')
-        unpack_data('A003',year,lastdate)
-        # TODO: Esse é um caso real onde pastas são criadas, mas não tem arquivo dentro?
-        # Devo procurar o ultimo arquivo com dados ou posso confiar nas pastas e que os arquivos tem conteudo?
-    except:
-        pass
+class stations_available(object):
+    def __init__(self):
+        self.dir = 'stations/stations'
+
+        self.header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
+        'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
+        'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
+
+
+    def station_names(self):
+        # Coluna 1 = Nome das estacoes = Pega o nome da cada pasta
+        self.stations_list = os.listdir('stations/stations')
+
+    def station_year(self, station):
+        # Coluna2 = Data recente
+        # abre pasta com o maior ano
+        self.station = station
+        self.path = '{}/{}'.format(self.dir, self.station)
+        years = os.listdir(self.path)
+        years = map(int,years)
+        self.lastest_year = -1
+        for year in years:
+            self.lastest_year = year if year > self.lastest_year else self.lastest_year
+
+        # Pegar uma lista de nome dos arquivos
+        # remove o .txt.zip
+    def station_date(self):
+        self.path = '{}/{}'.format(self.path, str(self.lastest_year))
+        dates = os.listdir(self.path)
+        self.latest_date = datetime(1800,1,1)
+        for date in dates:
+            try:
+                # converte o restante pra pd_datetime ou date(YYYY,MM,DD)
+                date = datetime.strptime(date[:-8], '%Y-%m-%d')
+                self.latest_date = date if date > self.latest_date else self.latest_date
+            except:
+                # por hora, vou ignorar datas bugadas (IE: 30/02/20XX)
+                pass
+        # pega o arquivo mais velho
+    
+    def station_hour(self, df):
+        '''
+        Abre um dataframe do banco de dados e tira a ultima horade registro
+        '''
+        self.latest_hour = df['HORA'].iloc[-1]
+
+    def station_latest(self):
+        self.latest_data = {
+            'station': self.station,
+            'date': self.latest_date,
+            'hour': self.latest_hour
+        }
+
+    def unpack_data(self,station,year,date):
+        # Coluna3 = Hora recente
+        # Abre o arquvio com a data mais recente
+        file_path = '{}/{}/{}/{}.txt.zip'.format(self.dir, station, year, date)
+
+        header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
+        'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
+        'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
+        station_df = pd.read_csv(file_path, header=None, delim_whitespace=True)
+        station_df.columns = header
+        return station_df
+
+        # converte a coluna ['HORA'] pra pd.to_datetime ou algo do tipo
+        # pega o valor mais recente OU pega ultima linha
+
+
+
+
+@error_handler
+def teste(disp):
+    disp.station_year(i)
+    disp.station_date()
+    df = disp.unpack_data(i, disp.lastest_year, datetime.strftime(disp.latest_date, '%Y-%m-%d'))
+    disp.station_hour(df)
+    disp.station_latest()
+    print(disp.latest_data)
+
+
+disp = stations_available()
+disp.station_names()
+for i in disp.stations_list:
+    teste(disp)
+
+
+# for station in station_name():
+#     try:    
+#         year, lastdate = station_date(station)
+#         lastdate = datetime.strftime(lastdate, '%Y-%m-%d')
+#         unpack_data(station,year,lastdate)
+#         # TODO: Esse é um caso real onde pastas são criadas, mas não tem arquivo dentro?
+#         # Devo procurar o ultimo arquivo com dados ou posso confiar nas pastas e que os arquivos tem conteudo?
+#     except:
+#         pass
