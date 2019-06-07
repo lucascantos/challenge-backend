5066031276f37494fcc5e089a9bd106d29d67a64
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-06-07 20:36:03 -0300

    New Branch first commit

--- stations.py
+++ stations.py
@@ -22,7 +22,6 @@ def error_handler(function):
     Função de tratamento dos erros da função:
     TODO: Aprender dar um argumento de entrada no decorador pra customizar os erros por função
     TODO: Depois disso, fazer o log com os erros
-    TODO 2: Adicionar um novo wrapper pra adiconar argumento
     '''
     def wrapper(*args, **kargs):
         try:
@@ -35,43 +34,35 @@ def error_handler(function):
 
 class stations_available(object):
     def __init__(self):
-
         self.dir = 'stations/stations'
 
         self.header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
         'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
         'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
 
-    def station_names(self):
-        '''
-        Lẽ todas as pastas e cria uma lista de estações.
-        '''
-        # Coluna 1 = Nome das estacoes = Pega o nome da cada pasta
-        station_list = os.listdir(self.dir)
-        for self.station in station_list:
-            yield self.station
 
-    def station_year(self):
-        '''
-        Dentro de uma pasta de estação, devolve o nome da pasta com o ultimo ano
-        '''
+    def station_names(self):
+        # Coluna 1 = Nome das estacoes = Pega o nome da cada pasta
+        self.stations_list = os.listdir('stations/stations')
+
+    def station_year(self, station):
         # Coluna2 = Data recente
         # abre pasta com o maior ano
-
+        self.station = station
         self.path = '{}/{}'.format(self.dir, self.station)
         years = os.listdir(self.path)
-        years = map(int, years).sort(reversed=True)
-        for self.latest_year in years:
-            yield self.latest_year
+        years = map(int,years)
+        self.lastest_year = -1
+        for year in years:
+            self.lastest_year = year if year > self.lastest_year else self.lastest_year
 
         # Pegar uma lista de nome dos arquivos
         # remove o .txt.zip
-        
     def station_date(self):
-        self.path = '{}/{}'.format(self.path, str(self.latest_year))
+        self.path = '{}/{}'.format(self.path, str(self.lastest_year))
         dates = os.listdir(self.path)
-        self.latest_date = datetime.strptime(dates[0][:-8], '%Y-%m-%d')
-        for date in dates[1:]:
+        self.latest_date = datetime(1800,1,1)
+        for date in dates:
             try:
                 # converte o restante pra pd_datetime ou date(YYYY,MM,DD)
                 date = datetime.strptime(date[:-8], '%Y-%m-%d')
@@ -79,38 +70,31 @@ class stations_available(object):
             except:
                 # por hora, vou ignorar datas bugadas (IE: 30/02/20XX)
                 pass
+        # pega o arquivo mais velho
     
-
     def station_hour(self, df):
         '''
-        Abre um dataframe do banco de dados e tira a ultima hora de registro
+        Abre um dataframe do banco de dados e tira a ultima horade registro
         '''
         self.latest_hour = df['HORA'].iloc[-1]
 
-
     def station_latest(self):
-        '''
-        Resultado final na forma de Dictionary
-        '''
-        self.latest_date = self.latest_date.replace(hour=self.latest_hour)
-        
         self.latest_data = {
             'station': self.station,
-            'date': self.latest_date
+            'date': self.latest_date,
+            'hour': self.latest_hour
         }
 
-    def unpack_data(self,station=None,year=None,date=None):
+    def unpack_data(self,station,year,date):
         # Coluna3 = Hora recente
         # Abre o arquvio com a data mais recente
-        if (station and year and date) == None:
-            station = self.station
-            year = self.latest_year
-            date = datetime.strftime(self.latest_date, '%Y-%m-%d')
-
         file_path = '{}/{}/{}/{}.txt.zip'.format(self.dir, station, year, date)
 
+        header = ['ESTACAO', 'LATITUDE', 'LONGITUDE', 'ALTITUDE', 'ANO', 'MES', 'DIA', 'HORA', 'TEMP', 'TMAX', 'TMIN',
+        'UR','URMAX' , 'URMIN' , 'TD' , 'TDMAX' , 'TDMIN' , 'PRESSAONNM' , 'PRESSAONNM_MAX' , 'PRESSAONNM_MIN',
+        'VELVENTO', 'DIRVENTO', 'VELVENTO_RAJADA', 'RADIACAO', 'PRECIPATACAO']
         station_df = pd.read_csv(file_path, header=None, delim_whitespace=True)
-        station_df.columns = self.header
+        station_df.columns = header
         return station_df
 
         # converte a coluna ['HORA'] pra pd.to_datetime ou algo do tipo
@@ -123,7 +107,7 @@ class stations_available(object):
 def teste(disp):
     disp.station_year(i)
     disp.station_date()
-    df = disp.unpack_data()
+    df = disp.unpack_data(i, disp.lastest_year, datetime.strftime(disp.latest_date, '%Y-%m-%d'))
     disp.station_hour(df)
     disp.station_latest()
     print(disp.latest_data)
