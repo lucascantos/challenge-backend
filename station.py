f44ffe6d6dca77603b6316a3cecd80f2141e6b1c
Lucas Cantos <lucas.cantos@somarmeteorologia.com.br>
2019-06-07 20:19:13 -0300

    Branch 1: Tentando usar yield nas coisas

--- station.py
+++ station.py
@@ -1,3 +1,11 @@
 '''
 Funçoes de consulta de estações
 '''
+import os
+dire = 'stations/stations'
+x = os.walk(dire)
+for i in x:
+    try:
+        print('{}: {}'.format(i[0], i[2][-1]))
+    except:
+        pass
